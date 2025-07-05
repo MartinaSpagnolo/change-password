8aff8581be3f7d46493730a1912dbe2fee77fba8
Martina <mar.spagnolo@gmail.com>
2025-07-05 21:13:01 +0200

    fix view

--- resources/views/filament/pages/change-password.blade.php
+++ resources/views/filament/pages/change-password.blade.php
@@ -1,13 +1,13 @@
 <x-filament-panels::page>
     <form wire:submit="save">
         {{ $this->form }}
-<div style="display: flex; justify-content: end; align-items: center">
-            <x-filament::button type="submit" style="width:150px; margin-top: 1.5rem" wire:loading.attr="disabled">
-                <x-filament::loading-indicator wire:loading class="h-5 w-5" /> Conferma
+
+        <div style="display: flex; justify-content: end; align-items: center">
+            <x-filament::button type="submit" style="width: 150px; margin-top: 1.5rem" wire:loading.attr="disabled">
+                <x-filament::loading-indicator wire:loading class="h-5 w-5" />
+                Conferma
             </x-filament::button>
         </div>
-       // <x-filament::button type="submit" style="width:150px; margin-top: 1.5rem" wire:loading.attr="disabled">
-       //     <x-filament::loading-indicator wire:loading class="h-5 w-5" /> Submit
-       // </x-filament::button>
     </form>
 </x-filament-panels::page>
+
